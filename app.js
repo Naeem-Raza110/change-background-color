a0611577dfe6f788202266e5ffdf53255aa076a2
Naeem-Raza110 <nraza7225@gmail.com>
2024-09-23 06:01:43 -0700

    changes in js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementById("colorButton");
     button.addEventListener("click", function() {
         // Generate a random color
-        var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        // Change background color of the body
+        let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = randomColor;
     });
 });
